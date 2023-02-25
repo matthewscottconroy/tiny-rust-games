76dfcb8206d222c8fa7a9a8803ff698b5b558716
Matthew Conroy <matthewconroy@pop-os.localdomain>
2023-02-25 15:30:48 -0500

    A basic game state machine has been added along with corresponding menus. The menus respond to keyboard presses. The play gamestate currently transitions immediately to the end game state. The next step is to implement the play state and instantiate a board.

--- tic-tac-toe/tic-tac-toe-brackets/src/main.rs
+++ tic-tac-toe/tic-tac-toe-brackets/src/main.rs
@@ -1,12 +1,67 @@
 use bracket_lib::prelude::*;
 use tic_tac_toe_lib::Board;
 
-struct State {}
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
+
+struct State {
+    mode: GameMode,
+}
+
+impl State {
+    fn play(&mut self, ctx: &mut BTerm) {
+        self.mode = GameMode::End;
+    }
+
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Tic-Tac-Toe");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn end_game(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "The Game has finished.");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello, Bracket Terminal!");
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::End => self.end_game(ctx),
+            GameMode::Playing => self.play(ctx),
+        }
+        /*
+                ctx.cls();
+                ctx.print(1, 1, "Hello, Bracket Terminal!");
+        */
     }
 }
 
@@ -14,5 +69,10 @@ fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Tic-Tac-Toe")
         .build()?;
-    main_loop(context, State {})
+    main_loop(
+        context,
+        State {
+            mode: GameMode::Menu,
+        },
+    )
 }
