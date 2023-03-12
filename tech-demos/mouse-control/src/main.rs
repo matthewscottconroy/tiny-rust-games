d5ac94056c62175fdd69a1ca2864a4101668e3c4
Matthew Conroy <matthewconroy@pop-os.localdomain>
2023-03-12 01:16:52 -0500

    Added right and left mouse press detection to the mouse-control tech demo. The cursor turns red or blue depending upon which button is being pressed.

--- tech-demos/mouse-control/src/main.rs
+++ tech-demos/mouse-control/src/main.rs
@@ -8,7 +8,19 @@ impl GameState for State {
         draw_batch.cls();
         let mouse_pos = INPUT.lock().mouse_tile(0);
 
-        draw_batch.print_color(mouse_pos, "X", ColorPair::new(WHITE_SMOKE, WHITE_SMOKE));
+        let is_left_pressed = INPUT.lock().is_mouse_button_pressed(0);
+        let is_right_pressed = INPUT.lock().is_mouse_button_pressed(1);
+        let mut cursor_color = WHITE_SMOKE;
+
+        if is_left_pressed {
+            cursor_color = RED;
+        }
+
+        if is_right_pressed {
+            cursor_color = BLUE;
+        }
+
+        draw_batch.print_color(mouse_pos, "X", ColorPair::new(cursor_color, cursor_color));
 
         draw_batch.submit(0).expect("Batch error");
         render_draw_buffer(ctx).expect("Render error");
