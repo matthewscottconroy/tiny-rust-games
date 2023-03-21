015a2fc68983484ad615bd25dbec36d8744aa5b2
Matthew Conroy <matthewconroy@pop-os.localdomain>
2023-03-21 18:46:26 -0400

    Added mouse position tracking and printing to the mouse_control demo.

--- tech-demos/mouse-control/src/main.rs
+++ tech-demos/mouse-control/src/main.rs
@@ -1,6 +1,9 @@
 use bracket_terminal::prelude::*;
 
-struct State {}
+struct State {
+    x: i32,
+    y: i32,
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
@@ -9,6 +12,7 @@ impl GameState for State {
         let mut draw_batch = DrawBatch::new();
         draw_batch.cls();
         let mouse_pos = INPUT.lock().mouse_tile(0);
+        let Point { x, y } = mouse_pos;
 
         let is_left_pressed = INPUT.lock().is_mouse_button_pressed(0);
         let is_right_pressed = INPUT.lock().is_mouse_button_pressed(1);
@@ -23,6 +27,11 @@ impl GameState for State {
         }
 
         draw_batch.print_color(mouse_pos, "X", ColorPair::new(cursor_color, cursor_color));
+        draw_batch.print_color(
+            Point { x: 1, y: 1 },
+            format!("x: {}, y: {}", x, y),
+            ColorPair::new(WHITE_SMOKE, BLACK),
+        );
 
         draw_batch.submit(0).expect("Batch error");
         render_draw_buffer(ctx).expect("Render error");
@@ -33,6 +42,6 @@ fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Mouse Control")
         .build()?;
-    let gs = State {};
+    let gs = State { x: 0, y: 0 };
     main_loop(context, gs)
 }
