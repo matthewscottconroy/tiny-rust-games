4d36107984bb9fb2292ef845329ba44785597a17
Matthew Conroy <matthewconroy@pop-os.localdomain>
2023-03-21 23:45:19 -0400

    bracket-lib tic-tac-toe can now accept mouse clicks for placing moves on the board.

--- tic-tac-toe/tic-tac-toe-brackets/src/main.rs
+++ tic-tac-toe/tic-tac-toe-brackets/src/main.rs
@@ -11,11 +11,69 @@ const BACKGROUND_COLOR: (u8, u8, u8) = WHITE_SMOKE;
 const FOREGROUND_COLOR: (u8, u8, u8) = SKY_BLUE;
 const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
 const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+const BOARD_OFFSET_X: i32 = 10;
 const BOARD_OFFSET_Y: i32 = 5;
-const BOARD_OFFSET_X: i32 = 17;
 
 pub trait Render {
     fn render(&mut self, row: usize, column: usize, ctx: &mut BTerm);
+}
+
+pub trait WorldGrid {
+    fn screen_to_world(&self, screen_x: i32, screen_y: i32) -> (i32, i32) {
+        (screen_x, screen_y)
+    }
+
+    fn world_to_screen(&self, world_x: i32, world_y: i32) -> (i32, i32) {
+        (world_x, world_y)
+    }
+
+    fn is_inside(&self, screen_x: i32, screen_y: i32) -> bool {
+        true
+    }
+}
+
+impl WorldGrid for TicTacToeGame {
+    fn screen_to_world(&self, screen_x: i32, screen_y: i32) -> (i32, i32) {
+        let screen_x_distance = screen_x - BOARD_OFFSET_X;
+        let screen_y_distance = screen_y - BOARD_OFFSET_Y;
+
+        let world_x_location = screen_x_distance / 2;
+        let world_y_location = screen_y_distance / 2;
+        println!(
+            "Inside screen to world: screen_x: {}, screen_y: {}, world_x: {}, world_y: {}",
+            screen_x, screen_y, world_x_location, world_y_location
+        );
+
+        (world_x_location, world_y_location)
+    }
+
+    fn world_to_screen(&self, world_x: i32, world_y: i32) -> (i32, i32) {
+        let screen_x = BOARD_OFFSET_X + world_x * 2;
+        let screen_y = BOARD_OFFSET_Y + world_y * 2;
+        println!(
+            "Inside world to Screen: world_x: {}, world_y: {}, screen_x:{}, screen_y:{}",
+            world_x, world_y, screen_x, screen_y
+        );
+
+        (screen_x, screen_y)
+    }
+
+    fn is_inside(&self, screen_x: i32, screen_y: i32) -> bool {
+        let width: i32 = self.get_width().try_into().unwrap();
+        let height: i32 = self.get_height().try_into().unwrap();
+
+        let (upper_x, upper_y) = self.world_to_screen(0, 0);
+        let (bottom_x, bottom_y) = self.world_to_screen(width - 1, height - 1);
+        let result = !(screen_x < upper_x
+            || screen_x > bottom_x
+            || screen_y < upper_y
+            || screen_y > bottom_y);
+        println!("screen_x: {}, screen_y: {}", screen_x, screen_y);
+        println!("upper_x: {}, upper_y: {}", upper_x, upper_y);
+        println!("bottom_x: {}, bottom_y: {}", bottom_x, bottom_y);
+        println!("is_inside: {}", result);
+        result
+    }
 }
 
 impl Render for TicTacToeGame {
@@ -25,11 +83,11 @@ impl Render for TicTacToeGame {
         let b_width = board.get_width();
 
         ctx.cls_bg(BACKGROUND_COLOR);
-        ctx.print_color_centered(5, FOREGROUND_COLOR, BACKGROUND_COLOR, "Tic-Tac-Toe!");
+        ctx.print_color(5, 2, FOREGROUND_COLOR, BACKGROUND_COLOR, "Tic-Tac-Toe");
         let mut i = BOARD_OFFSET_Y;
 
         for line in self.get_pretty_board().split('\n') {
-            ctx.print_color_centered(i + BOARD_OFFSET_Y, FOREGROUND_COLOR, BACKGROUND_COLOR, line);
+            ctx.print_color(BOARD_OFFSET_X, i, FOREGROUND_COLOR, BACKGROUND_COLOR, line);
             i += 1;
         }
     }
@@ -52,7 +110,6 @@ impl State {
     fn play(&mut self, ctx: &mut BTerm) {
         ctx.mouse_visible = true;
         ctx.cls_bg(WHITE_SMOKE);
-
         self.frame_time += ctx.frame_time_ms;
 
         if self.frame_time > FRAME_DURATION {
@@ -63,9 +120,18 @@ impl State {
 
         let mouse_pos = INPUT.lock().mouse_tile(0);
         let mut cursor_color = WHITE;
+        let Point { x, y } = mouse_pos;
 
         let is_left_pressed = INPUT.lock().is_mouse_button_pressed(0);
         let mut draw_batch = DrawBatch::new();
+
+        /*
+                draw_batch.print_color(
+                    mouse_pos,
+                    &format!("Mouse position: {:?}", mouse_pos),
+                    ColorPair::new(BACKGROUND_COLOR, FOREGROUND_COLOR),
+                );
+        */
 
         if is_left_pressed {
             self.was_left_mouse_pressed = true;
@@ -73,7 +139,14 @@ impl State {
             draw_batch.print_color(mouse_pos, " ", ColorPair::new(cursor_color, cursor_color));
         } else {
             if self.was_left_mouse_pressed {
-                self.game.take_turn(1, 1);
+                let (world_x, world_y) = self.game.screen_to_world(x, y);
+
+                if self.game.is_inside(x, y) {
+                    println!("I'm inside");
+                    self.game
+                        .take_turn(world_y.try_into().unwrap(), world_x.try_into().unwrap());
+                }
+
                 self.was_left_mouse_pressed = false;
             }
         }
