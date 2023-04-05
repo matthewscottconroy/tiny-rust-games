748a67375f44ea1e9f0db4434e60f707c48b7ca4
Matthew Conroy <matthewconroy@pop-os.localdomain>
2023-04-05 03:13:38 -0400

    Added a hello world bevy tech demo.

---
+++ tech-demos/bevy/hello-world/src/main.rs
@@ -0,0 +1,31 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn main() {
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
+}
+
+fn hello_world() {
+    println!("Hello, World!");
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Sam".to_string())));
+    commands.spawn((Person, Name("Charlie".to_string())));
+    commands.spawn((Person, Name("David".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("Hello {}!", name.0);
+    }
+}
